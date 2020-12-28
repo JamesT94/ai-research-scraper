f4ce87452a93c54231123741118b3cd0397f2f57
JamesT94 <jamest9401@gmail.com>
2020-12-28 15:36:56 +0000

    1 page working

--- try-again.py
+++ try-again.py
@@ -23,19 +23,32 @@ def get_soup(url):
 
 
 def get_doc_list(search_soup):
+    results = []
     links = search_soup.find_all('a', class_='result-heading')
     for link in links:
-        print(link.text)
-        print(link['href'])
+        results.append(link['href'])
+    return results
 
 
-# url = create_url("https://www.gartner.com/en/search?keywords=<Search_Term>&page=<Page_Num>", '1', 'AI')
+def trim_reports(link_list, report_bool):
+    substring = '/en/documents/'
+    for link in link_list:
+        if substring in link:
+            report_bool.append(1)
+        else:
+            report_bool.append(0)
+    reports = list(zip(link_list, report_bool))
+    report_list = pd.DataFrame(reports, columns=['Link', 'Report_Bool'])
+    report_list = report_list[report_list.Report_Bool == 1]
+    report_list.drop(['Report_Bool'], axis=1, inplace=True)
 
-doc_page_url = 'https://www.gartner.com/en/documents/2020/12/3993877-predicts-2021-operational-ai-infrastructure-and' \
-               '-enabling'
+    return report_list
 
-columns = ['Title', 'Date', 'Doc ID', 'Analyst(s)', 'Summary', 'Link']
-results_df = pd.DataFrame(columns=columns)
+
+def create_output_doc():
+    columns = ['Title', 'Date', 'Doc ID', 'Analyst(s)', 'Summary', 'Link']
+    dataframe = pd.DataFrame(columns=columns)
+    return dataframe
 
 
 def add_page_content(url, dataframe):
@@ -52,5 +65,29 @@ def add_page_content(url, dataframe):
     return dataframe.append(new_row, ignore_index=True)
 
 
-results_df = add_page_content(doc_page_url, results_df)
-print(results_df)
+def visit_each_page(report_list, full_results):
+    for link in report_list['Link']:
+        full_results = add_page_content(link, full_results)
+    return full_results
+
+
+search_url = create_url("https://www.gartner.com/en/search?keywords=<Search_Term>&page=<Page_Num>", '1', 'AI')
+
+results_df = create_output_doc()
+
+search_soup = get_soup(search_url)
+
+link_list = get_doc_list(search_soup)
+
+report_bool = []
+report_list = trim_reports(link_list, report_bool)
+
+results_df = visit_each_page(report_list, results_df)
+results_df.to_csv('output.csv')
+
+
+
+
+
+
+
